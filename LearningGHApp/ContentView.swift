99b44e52798cf56cb3efa1398d0005d7308dbb16
K Volz <kvolz324@gmail.com>
2021-04-13 14:33:22 -0500

    Initial Commit

---
+++ LearningGHApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningGHApp
+//
+//  Created by Katie Volz on 4/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningGHApp/LearningGHAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHAppApp.swift
+//  LearningGHApp
+//
+//  Created by Katie Volz on 4/13/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
